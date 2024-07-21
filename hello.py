49a3a103e213f97273b82b8edea8992fbef481d0
Fedor <bakhvalov.fedya@yandex.ru>
2024-07-21 20:38:25 +0300

    he

--- hello.py
+++ hello.py
@@ -1,13 +1,5 @@
 print('hello_world')
 
 for c in range(1000):
-    print(c**2)
+    pass
 
-
-class Fib:
-    def __init__(self,number):
-        self.number=number
-
-
-print(Fib(10).number)
-print('nuic')
